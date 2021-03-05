d671942d587f22c167affeff4fc1e2033b310bdd
Your Name <gong@vip.com>
2021-03-05 11:57:29 +0800

    docs(类注释): 修改类注释

--- src/main/java/com/shengtianyizi/container/RoomsContainer.java
+++ src/main/java/com/shengtianyizi/container/RoomsContainer.java
@@ -1,9 +1,6 @@
 package com.shengtianyizi.container;
 
-import lombok.Synchronized;
 import lombok.extern.slf4j.Slf4j;
-
-import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 
